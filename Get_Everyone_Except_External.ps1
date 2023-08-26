2679c46b5abc8ccdde5996e16010dac95f7ed751
Jason Baxter <jasonwbaxter@hotmail.com>
2023-08-26 15:32:55 +0200

    Added set site collection admin

--- Get_Everyone_Except_External.ps1
+++ Get_Everyone_Except_External.ps1
@@ -1,5 +1,5 @@
 # Connect to SharePoint Online
-Connect-SPOService -Url https://900904-admin.sharepoint.com
+Connect-SPOService -Url https://<tenant>-admin.sharepoint.com
 
 # Specify the group's ID
 $groupId = "spo-grid-all-users/eed97d6b-5444-44cd-9eae-85380aa53722"
@@ -8,7 +8,7 @@ $sites= import-csv -literalpath c:\temp\groupconnectedsites.csv
 $collection=@()
 Foreach ($site in $sites ){
 # Set Admin as Sitecollection Admin
-Set-SPOUser -Site $site.Url -LoginName jabaxt@900904.onmicrosoft.com -IsSiteCollectionAdmin $true
+#Set-SPOUser -Site $site.Url -LoginName <adminuser> -IsSiteCollectionAdmin $true | Out-Null
 
 # Get information about the group 
 $group = Get-SPOSiteGroup $site.url 
@@ -23,8 +23,8 @@ foreach($g in $group){
              }
     
 }
-
-Set-SPOUser -Site $site.Url -LoginName jabaxt@900904.onmicrosoft.com -IsSiteCollectionAdmin $false
+#Remove admin as sitecollection admin.
+#Set-SPOUser -Site $site.Url -LoginName <adminuser> -IsSiteCollectionAdmin $false | Out-Null
 } 
 
 # Disconnect from SharePoint Online
